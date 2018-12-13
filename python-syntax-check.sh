76ab51cd8064f726ccf22e1801d749fb01cde11e
Rui Gu <rui@covalent.io>
2018-12-13 14:48:47 -0800

    Make syntax checker take a folder as an argument

--- python-syntax-check.sh
+++ python-syntax-check.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 SCRIPTS_DIR="$(dirname ${0})"
 
+if [ -z "$1" ]
+  then
+    echo "Please provide the source code folder location."
+    echo "Example: ./python-syntax-check.sh SOURCE_DIR"
+    exit 1
+fi
+
+SOURCE_DIR="$1"
+
 echo "Syntax check for all python code..."
 while read f; do
+    echo $f
     if ! python -m py_compile "${f}"; then
         echo "Error: Please check ${f} for Python 2 syntax errors"
         exit 1
@@ -11,4 +21,4 @@ while read f; do
         echo "Error: Please check ${f} for Python 3 syntax errors"
         exit 1
     fi
-done < <(find "${SCRIPTS_DIR}/cluster-diagnosis" -name "*.py" -prune -type f)
+done < <(find "${SOURCE_DIR}" -name "*.py" -prune -type f)
